a6b31b91f2efe84c149de872bf2191a74fcd3ad8
Sanjeet Mistry <sanjeetm@axioned.com>
2021-11-22 20:28:02 +0530

    Add Complete JS

---
+++ js/home.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let wrapper = document.querySelector(".wrapper");
+    let filters = wrapper.querySelectorAll(".filter > ul > li");
+    let filterSelectedArray = [true, false, false, false, false, false, false];
+    let imagesArray = [];
+    let jsonData = [];
+
+    for(let i = 0; i < 16; i++) {
+        imagesArray[i] = true;
+    }
+
+    for(let filter of filters) {
+        filter.addEventListener("click", function() {
+            changeFilter(this, this.getAttribute("data-id"));
+        });
+    }
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "/json/content.json");
+    xhttp.send();
+    xhttp.onload = function() {
+        jsonData = JSON.parse(this.responseText);
+        console.log(jsonData);
+    }
+
+    function changeFilter(element, id) {
+        id = parseInt(id);
+        if(filterSelectedArray[id]) {
+            element.classList.remove("selected");
+        }
+        else {
+            element.classList.add("selected");
+        }
+        filterSelectedArray[id] = !filterSelectedArray[id];
+        calculateIntersectionOnFilterChange();
+    }
+
+    function calculateIntersectionOnFilterChange() {
+        if(filterSelectedArray[0]) {
+            updateImage(true);
+        }
+        else {
+            let allArrays = [], count = 0;
+            for(let i = 0; i < filterSelectedArray.length; i++) {
+                if(filterSelectedArray[i]) {
+                    allArrays[count++] = jsonData[i-1].images;
+                }
+            }
+
+            if(!allArrays.length) {
+                updateImage(false, "");
+                return;
+            }
+            let result = allArrays.shift().filter(function(v) {
+                return allArrays.every(function(a) {
+                    return a.indexOf(v) !== -1;
+                });
+            });
+
+            updateImage(false, result);
+        }
+    }
+
+    function updateImage(allTrue, result) {
+        let liArray = wrapper.querySelectorAll(".content > li"); 
+        
+        if(result == "") {
+            for(let i = 0; i < imagesArray.length; i++) {
+                imagesArray[i] = false;
+            }
+        }
+        else {
+            if(allTrue) {
+                for(let i = 0; i < imagesArray.length; i++) {
+                    imagesArray[i] = true;
+                }
+            }
+            else {
+                let j = 0;
+                for(let i = 0; i < imagesArray.length; i++) {
+                    if(i == (result[j] -1)) {
+                        imagesArray[i] = true;
+                        j++;
+                    }
+                    else {
+                        imagesArray[i] = false;
+                    }
+                }
+            }
+        }
+
+        for(let i = 0; i < imagesArray.length; i++) {
+            if(imagesArray[i]) {
+                if(liArray[i].classList[0]) {
+                    liArray[i].classList.remove("display-none");
+                }
+            }
+            else {
+                if(!liArray[i].classList[0]) {
+                    liArray[i].classList.add("display-none");
+                }
+            }
+        }
+    }
+});
